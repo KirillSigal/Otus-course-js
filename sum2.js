740cb2cf4011bab6cecc53e0dd7264f112d597c4
TurboDeadlineMaster <63202783+TurboDeadlineMaster@users.noreply.github.com>
2020-04-15 21:05:28 +0300

    Add files via upload
    First homework for Otus

---
+++ sum2.js
@@ -0,0 +1,16 @@
+function sum(a) {
+
+  let currentSum = a;
+
+  function f(b) {
+    currentSum += b;
+    return f;
+  }
+
+  f.toString = function() {
+    return currentSum;
+  };
+
+  return f;
+}
+alert(sum(1)(-5)(6));
